283da3fafa2dc3b043824c1f451c61c96e77b193
Marius Greuel <greuelm@mgtek.com>
2020-10-06 21:04:13 +0200

    Add Windows version resource to avrdude project

---
+++ msvc/version.h
@@ -0,0 +1,14 @@
+//
+// version.h
+// Copyright (C) 2020 Marius Greuel. All rights reserved.
+//
+
+#define PRD_MAJOR       6
+#define PRD_MINOR       3
+#define PRD_BUILD       1
+#define PRD_REVISION    0
+
+#define VER_MAJOR       PRD_MAJOR
+#define VER_MINOR       PRD_MINOR
+#define VER_BUILD       PRD_BUILD
+#define VER_REVISION    PRD_REVISION
